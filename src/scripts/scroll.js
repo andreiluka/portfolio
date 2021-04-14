b85c2b3d1872fe714478657a95e098b3563e00c8
andrey <andreiluka@ya.ru>
2021-04-14 19:32:35 +0300

    добавил модальные окна, изменил стили под style guide

---
+++ src/scripts/scroll.js
@@ -0,0 +1,24 @@
+console.log('this is the scroll module');
+
+function scrollSmooth(section) {
+   document.querySelector(section).scrollIntoView({
+      behavior: 'smooth'
+   });
+}
+
+let heroScroll = document.querySelector('.hero__scroll');
+heroScroll.addEventListener('click', (e) => {
+   e.preventDefault();
+   scrollSmooth('.about');
+});
+
+let sections = ['hero', 'about', 'skills', 'portfolio', 'reviews', 'write-me'];
+for (let section of sections) {
+   
+   for (let sel of document.querySelectorAll("a[href='#"+section+"']")) {
+      sel.addEventListener('click', (e) => {
+         e.preventDefault();
+         scrollSmooth('#'+section);
+      });
+   }
+}
