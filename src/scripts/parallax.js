7ffa2166197a02c7ed14a4e4f0a18f28bafbf8dc
andrey <andreiluka@ya.ru>
2021-05-06 14:22:28 +0300

    добавил parallax на первый фон (горы), модуль parallax

---
+++ src/scripts/parallax.js
@@ -0,0 +1,19 @@
+console.log('this is the parallax module');
+
+const parallax = document.querySelector('.bg-mountains');
+const layers = parallax.children;
+
+function moveLayers(wScroll) {
+
+   Array.from(layers).forEach(layer => {
+      const divider = layer.dataset.speed;
+      const strafe = wScroll * divider / 10;
+
+      layer.style.transform = `translateY(-${strafe}%)`;
+   });
+}
+
+window.addEventListener('scroll', e => {
+   const wScroll = window.pageYOffset;
+   moveLayers(wScroll);
+})
