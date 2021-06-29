64c27468b2a345d6f53c5cfacf5a04c74c68938f
andrey <andreiluka@ya.ru>
2021-06-29 21:54:25 +0300

    добавил skills-widget в js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
+import "./scripts/reviews-slider";
 import "./scripts/skills";
 import "./scripts/scroll";
 import "./scripts/parallax";
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,34 @@
 console.log('this is the skills module');
+
+import Vue from 'vue';
+
+const skill = {
+   template: 'skill'
+}
+
+const row = {
+   template: '#skills-row',
+   props: {
+      skill: Object
+   },
+   components: {
+      skill
+   }
+}
+
+new Vue ({
+   el: '#skills-widget',
+   template: '#skills-list',
+   components: {
+      row
+   },
+   data() {
+      return {
+         skills: []
+      }
+   },
+   created() {
+      const data = require('../data/skills.json');
+      this.skills = data;
+   }
+});
